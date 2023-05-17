1c9f121dcfe86336d1fe3daf4dfe48b5378545b1
joshnwosu <joshuanwosu078@gmail.com>
2023-05-18 00:00:42 +0100

    rearranged some stuff

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import SwipableList from "../components/layouts/ListLayout";
 
 export default function SettingsScreen() {
   return (
     <View style={{ flex: 1, paddingVertical: 30 }}>
       <Text>Settings</Text>
+      <SwipableList />
     </View>
   );
 }
