fa7ddd3425c792ccd5925b3e3eff6db422dd5a1c
joshnwosu <joshuanwosu078@gmail.com>
2023-05-20 01:46:01 +0100

    some changes

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,13 +1,15 @@
 import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import React from "react";
 import SwipableList from "../components/layouts/ListLayout";
+import BulletLayout from "../components/layouts/BulletLayout";
 
 export default function SettingsScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <ScrollView contentContainerStyle={{ flex: 1 }}>
         <Text>Settings</Text>
-        <SwipableList />
+        {/* <SwipableList /> */}
+        <BulletLayout />
       </ScrollView>
     </SafeAreaView>
   );
