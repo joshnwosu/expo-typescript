c3f913b4868c35fb67e516c9cf8f1b2310bfd4f0
joshnwosu <joshuanwosu078@gmail.com>
2023-05-20 12:24:15 +0100

    splashscreen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,5 @@
 import "react-native-gesture-handler";
 import "react-native-reanimated";
-
-// import { useContext, useEffect } from "react";
-// import { StatusBar } from "expo-status-bar";
-// import { View, StyleSheet, useColorScheme } from "react-native";
-// import AppNavigator from "./src/navigation/AppNavigator";
-// import { DefaultTheme, DarkTheme } from "@react-navigation/native";
-// import { SafeAreaProvider } from "react-native-safe-area-context";
-
 import { Provider } from "react-redux";
 import { store } from "./src/app/store";
 import { LogBox } from "react-native";
@@ -17,27 +9,6 @@ import Root from "./src/components/root/Root";
 LogBox.ignoreAllLogs(); // ignore all annoying warnings.
 
 const App: React.FC = () => {
-  // const scheme = useColorScheme();
-
-  // const CustomDefaultTheme = {
-  //   ...DefaultTheme,
-  //   colors: {
-  //     ...DefaultTheme.colors,
-  //     activeColor: "#5d68f9",
-  //     inActiveColor: "#6d6f7a",
-  //     // add custom color
-  //   },
-  // };
-
-  // const CustomDarkTheme = {
-  //   ...DarkTheme,
-  //   colors: {
-  //     ...DarkTheme.colors,
-  //     activeColor: "#5d68f9",
-  //     inActiveColor: "#6d6f7a",
-  //   },
-  // };
-
   return (
     <Provider store={store}>
       <ThemeProvider>
