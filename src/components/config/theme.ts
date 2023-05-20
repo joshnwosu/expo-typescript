6331866b9bb0d1acea3e051f6691cb74e2a459ff
joshnwosu <joshuanwosu078@gmail.com>
2023-05-20 04:47:28 +0100

    update: update theme colors

--- src/components/config/theme.ts
+++ src/components/config/theme.ts
@@ -1,24 +1,28 @@
 export const Theme = {
-  colors: {
-    light: {
-      primary: "red",
-      text: "blue",
-      background: "green",
-      card: "purple",
-      notification: "orange",
-      border: "brown",
-      activeColor: "#5d68f9",
-      inActiveColor: "#6d6f7a",
+  light: {
+    dark: false,
+    colors: {
+      background: "#f2f2f2" || "rgb(242, 242, 242)",
+      border: "#d8d8d8" || "rgb(216, 216, 216)",
+      card: "#ffffff" || "rgb(255, 255, 255)",
+      notification: "#ff3b30" || "rgb(255, 59, 48)",
+      primary: "#007aff" || "rgb(0, 122, 255)",
+      text: "#1c1c1e" || "rgb(28, 28, 30)",
+      activeColor: "#5d68f9" || "rgb(93, 104, 249)",
+      inActiveColor: "#6d6f7a" || "rgb(109, 111, 122)",
     },
-    dark: {
-      primary: "white",
-      text: "black",
-      background: "pink",
-      card: "magenta",
-      notification: "chocolate",
-      border: "tomato",
-      activeColor: "#5d68f9",
-      inActiveColor: "#6d6f7a",
+  },
+  dark: {
+    dark: true,
+    colors: {
+      background: "#010101" || "rgb(1, 1, 1)",
+      border: "#272729" || "rgb(39, 39, 41)",
+      card: "#121212" || "rgb(18, 18, 18)",
+      notification: "rgb(255, 69, 58)",
+      primary: "#0a84ff" || "rgb(10, 132, 255)",
+      text: "#e5e5e7" || "rgb(229, 229, 231)",
+      activeColor: "#5d68f9" || "rgb(93, 104, 249)",
+      inActiveColor: "#6d6f7a" || "rgb(109, 111, 122)",
     },
   },
 };
