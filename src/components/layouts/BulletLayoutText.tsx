2d528b79093cf3d1e0243cb12a8e4807b7266dd5
joshnwosu <joshuanwosu078@gmail.com>
2023-05-21 09:11:52 +0100

    Bullet Layout

---
+++ src/components/layouts/BulletLayoutText.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function BulletLayoutText() {
+  return (
+    <View>
+      <Text>BulletLayoutText</Text>
+    </View>
+  );
+}
