ab672f868a3b116dc34c52cf961afe43109ed579
joshnwosu <joshuanwosu078@gmail.com>
2023-05-22 01:28:48 +0100

    update

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -1,15 +1,24 @@
 import { View } from "react-native";
 import React from "react";
 import LottieView from "lottie-react-native";
+import BulletLayout from "../components/layouts/BulletEditLayout";
 
 export default function NotificationScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <LottieView
+    <View
+      style={{
+        flex: 1,
+        // justifyContent: "center",
+        // alignItems: "center",
+        marginTop: 50,
+      }}
+    >
+      {/* <LottieView
         autoPlay
         loop
         source={require("../../assets/lottie/landingAnimation.json")}
-      />
+      /> */}
+      <BulletLayout />
     </View>
   );
 }
