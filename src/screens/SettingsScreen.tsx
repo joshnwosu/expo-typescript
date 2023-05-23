27ad94fc76078ec9010a2e4f8a7a89a696a8e518
joshnwosu <joshuanwosu078@gmail.com>
2023-05-23 16:25:18 +0100

    update

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -6,16 +6,30 @@ import {
   ScrollView,
   Button,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import ListLayout from "../components/layouts/ListLayout";
 import ScreenLayout from "../components/layouts/ScreenLayout";
 import BulletEditLayout from "../components/layouts/BulletEditLayout";
 import * as Speech from "expo-speech";
+import * as Permissions from "expo-permissions";
 
 export default function SettingsScreen() {
+  useEffect(() => {
+    getPermissions();
+  }, []);
+
+  const getPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+
+    if (status !== "granted") {
+      console.log("Speech permissions not granted");
+    }
+  };
+
   const speak = () => {
-    const say1: any = 1;
-    Speech.speak(say1);
+    const text = "Hello, world!"; // Replace with the desired text to speak
+
+    Speech.speak(text);
   };
   return (
     <SafeAreaView style={{ flex: 1 }}>
