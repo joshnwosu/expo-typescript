468db414d2d71abca8023166e7d992a379782404
joshnwosu <joshuanwosu078@gmail.com>
2023-05-23 14:41:38 +0100

    update

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,15 +1,28 @@
-import { View, Text, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  Button,
+} from "react-native";
 import React from "react";
 import ListLayout from "../components/layouts/ListLayout";
 import ScreenLayout from "../components/layouts/ScreenLayout";
 import BulletEditLayout from "../components/layouts/BulletEditLayout";
+import * as Speech from "expo-speech";
 
 export default function SettingsScreen() {
+  const speak = () => {
+    const say1: any = 1;
+    Speech.speak(say1);
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* <ListLayout /> */}
       {/* <DragList /> */}
       {/* <BulletEditLayout /> */}
+      <Button onPress={speak} title="Speak" />
     </SafeAreaView>
   );
 }
