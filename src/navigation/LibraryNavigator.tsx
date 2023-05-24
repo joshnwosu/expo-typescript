2f669be48d6898720246d387d90506ad2866dad1
joshnwosu <joshuanwosu078@gmail.com>
2023-05-24 20:57:44 +0100

    update: made some changes

--- src/navigation/LibraryNavigator.tsx
+++ src/navigation/LibraryNavigator.tsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { View, TouchableOpacity } from "react-native";
 import LibraryScreen from "../screens/LibraryScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StackNavigatorWrapper from "./StackNavigatorWrapper";
 import FoldersScreen from "../screens/FoldersScreen";
 import LabelScreen from "../screens/LabelScreen";
+import SvgIcon from "../components/common/icons";
 
 const Stack = createNativeStackNavigator();
 
-const LibraryNavigator = () => {
+const LibraryNavigator = ({ navigation }) => {
   return (
     <StackNavigatorWrapper>
       <Stack.Screen
         name="Lib"
         options={{
           headerTitle: "Library",
+          headerRight(props) {
+            return (
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <TouchableOpacity style={{ marginLeft: 20 }}>
+                  <SvgIcon
+                    icon="notification"
+                    width={20}
+                    height={20}
+                    fill="#FFFFFF"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{ marginLeft: 20 }}
+                  onPress={() => navigation.navigate("Settings")}
+                >
+                  <SvgIcon
+                    icon="settings"
+                    width={20}
+                    height={20}
+                    fill="#FFFFFF"
+                  />
+                </TouchableOpacity>
+              </View>
+            );
+          },
         }}
         component={LibraryScreen}
       />
