e5eeaab199ddcb72b31c309a50ada7710395ca56
joshnwosu <joshuanwosu078@gmail.com>
2023-05-25 02:08:41 +0100

    update

--- src/navigation/BottomNavigator.tsx
+++ src/navigation/BottomNavigator.tsx
@@ -1,35 +1,24 @@
 import React from "react";
-import { useTheme } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import BottomTabBar from "../components/navigation/BottomTabBar";
 import LibraryNavigator from "./LibraryNavigator";
 import SearchNavigator from "./SearchNavigator";
 import SettingsNavigator from "./SettingsNavigator";
-import RightHeaderIcon from "../components/layouts/RightHeaderIcon";
 
 const Tab = createBottomTabNavigator();
 
+const EditComponent = () => <></>;
+
 const BottomNavigator = () => {
-  const { colors } = useTheme();
   return (
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
-        // headerStyle: {
-        //   backgroundColor: colors.background,
-        // },
-        headerShadowVisible: false,
-        // headerTitleStyle: {
-        //   fontSize: 20,
-        //   fontWeight: "600",
-        // },
-        // headerTitleAlign: "center",
-        headerRight: () => <RightHeaderIcon />,
       }}
       tabBar={(props) => <BottomTabBar {...props} />}
     >
       <Tab.Screen name="Library" component={LibraryNavigator} />
-      <Tab.Screen name="Edit" component={LibraryNavigator} />
+      <Tab.Screen name="Edit" component={EditComponent} />
       <Tab.Screen name="Search" component={SearchNavigator} />
       <Tab.Screen name="Settings" component={SettingsNavigator} />
     </Tab.Navigator>
