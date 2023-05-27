eb7aa0b0e8d3ae2e972e3fc4d729e129199a9dbf
joshnwosu <joshuanwosu078@gmail.com>
2023-05-27 15:08:00 +0100

    update

--- src/components/common/icons/HomeIcon.tsx
+++ src/components/common/icons/HomeIcon.tsx
@@ -3,17 +3,17 @@ import Svg, { SvgProps, Path } from "react-native-svg";
 interface Props extends SvgProps {
   fill?: string | undefined;
 }
-const HomeIcon: React.FC<Props> = ({ fill, ...props }: SvgProps) => (
+const HomeIcon: React.FC<Props> = ({
+  fill = "#292D32",
+  ...props
+}: SvgProps) => (
   <Svg viewBox="0 0 24 24" width={24} height={24} fill="none" {...props}>
     <Path
-      fill={fill ?? "#292D32"}
+      fill={fill}
       d="M10.07 2.82 3.14 8.37c-.78.62-1.28 1.93-1.11 2.91l1.33 7.96c.24 1.42 1.6 2.57 3.04 2.57h11.2c1.43 0 2.8-1.16 3.04-2.57l1.33-7.96c.16-.98-.34-2.29-1.11-2.91l-6.93-5.54c-1.07-.86-2.8-.86-3.86-.01Z"
       opacity={0.4}
     />
-    <Path
-      fill={fill ?? "#292D32"}
-      d="M12 15.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"
-    />
+    <Path fill={fill} d="M12 15.5a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
   </Svg>
 );
 export default HomeIcon;
