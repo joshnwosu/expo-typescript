0e13699fc2cc25077552c959c4fba9f730102636
joshnwosu <joshuanwosu078@gmail.com>
2023-05-28 07:13:58 +0100

    update: layout color

--- src/components/widgets/settings/appearance/theme/LayoutColor.tsx
+++ src/components/widgets/settings/appearance/theme/LayoutColor.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React, { useContext, useState } from "react";
 import ThemeContext from "../../../../context/ThemeContext";
+import GroupCard from "../../../../layouts/group-card/GroupCard";
 
 interface LayoutProp {
   name: "System" | "Light" | "Dark";
@@ -30,78 +31,83 @@ const LayoutColor = () => {
     theme: { colors },
   } = useContext(ThemeContext);
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-around",
-        padding: 20,
-      }}
+    <GroupCard
+      title="Accent color"
+      description="Change the main accent of the app. This will not change the color of existing tasks."
     >
-      {layout.map(({ name, backgroundColor, options }, index) => {
-        return (
-          <View style={{ flex: 1, alignItems: "center" }}>
-            <TouchableOpacity
-              activeOpacity={1}
-              key={name}
-              style={{
-                width: "100%",
-                height: 60,
-                borderRadius: 10,
-                padding: 10,
-              }}
-              onPress={() => setSelected(name)}
-            >
-              {options ? (
-                <View
-                  style={{
-                    flexDirection: "row",
-                    flex: 1,
-                    borderRadius: 10,
-                    overflow: "hidden",
-                    borderWidth: 2,
-                    borderColor: name === selected ? "tomato" : colors.border,
-                  }}
-                >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-around",
+          padding: 20,
+        }}
+      >
+        {layout.map(({ name, backgroundColor, options }, index) => {
+          return (
+            <View style={{ flex: 1, alignItems: "center" }}>
+              <TouchableOpacity
+                activeOpacity={1}
+                key={name}
+                style={{
+                  width: "100%",
+                  height: 60,
+                  borderRadius: 10,
+                  padding: 10,
+                }}
+                onPress={() => setSelected(name)}
+              >
+                {options ? (
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      flex: 1,
+                      borderRadius: 10,
+                      overflow: "hidden",
+                      borderWidth: 2,
+                      borderColor: name === selected ? "tomato" : colors.border,
+                    }}
+                  >
+                    <View
+                      style={{
+                        flex: 1,
+                        backgroundColor: options[0],
+                      }}
+                    />
+                    <View
+                      style={{
+                        flex: 1,
+                        backgroundColor: options[1],
+                      }}
+                    />
+                  </View>
+                ) : (
                   <View
                     style={{
                       flex: 1,
-                      backgroundColor: options[0],
+                      borderRadius: 10,
+                      backgroundColor,
+                      borderWidth: 2,
+                      borderColor: name === selected ? "tomato" : colors.border,
                     }}
                   />
-                  <View
-                    style={{
-                      flex: 1,
-                      backgroundColor: options[1],
-                    }}
-                  />
-                </View>
-              ) : (
-                <View
-                  style={{
-                    flex: 1,
-                    borderRadius: 10,
-                    backgroundColor,
-                    borderWidth: 2,
-                    borderColor: name === selected ? "tomato" : colors.border,
-                  }}
-                />
-              )}
-            </TouchableOpacity>
-            <Text
-              style={{
-                color: colors.text,
-                textTransform: "capitalize",
-                fontWeight: "400",
-                fontSize: 14,
-              }}
-            >
-              {name}
-            </Text>
-          </View>
-        );
-      })}
-    </View>
+                )}
+              </TouchableOpacity>
+              <Text
+                style={{
+                  color: colors.text,
+                  textTransform: "capitalize",
+                  fontWeight: "400",
+                  fontSize: 14,
+                }}
+              >
+                {name}
+              </Text>
+            </View>
+          );
+        })}
+      </View>
+    </GroupCard>
   );
 };
 
