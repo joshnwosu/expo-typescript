3a83db27ebc9d5524665b3027449f8a6952309c7
joshnwosu <joshuanwosu078@gmail.com>
2023-05-28 06:33:25 +0100

    update

--- src/components/widgets/settings/appearance/theme/LayoutColor.tsx
+++ src/components/widgets/settings/appearance/theme/LayoutColor.tsx
@@ -1,17 +1,18 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import ThemeContext from "../../../../context/ThemeContext";
 
 interface LayoutProp {
   name: "System" | "Light" | "Dark";
   backgroundColor: "Automatic" | "#FFFFFF" | "#000000";
-  colors?: ["#FFFFFF", "#000000"];
+  options?: ["#FFFFFF", "#000000"];
 }
 
 const layout: LayoutProp[] = [
   {
     name: "System",
     backgroundColor: "Automatic",
-    colors: ["#FFFFFF", "#000000"],
+    options: ["#FFFFFF", "#000000"],
   },
   {
     name: "Light",
@@ -25,6 +26,9 @@ const layout: LayoutProp[] = [
 
 const LayoutColor = () => {
   const [selected, setSelected] = useState<LayoutProp["name"]>(layout[0].name);
+  const {
+    theme: { colors },
+  } = useContext(ThemeContext);
   return (
     <View
       style={{
@@ -34,55 +38,67 @@ const LayoutColor = () => {
         padding: 20,
       }}
     >
-      {layout.map(({ name, backgroundColor, colors }, index) => {
+      {layout.map(({ name, backgroundColor, options }, index) => {
         return (
-          <TouchableOpacity
-            activeOpacity={1}
-            key={name}
-            style={{
-              flex: 1,
-              height: 60,
-              borderRadius: 10,
-              padding: 10,
-            }}
-            onPress={() => setSelected(name)}
-          >
-            {colors ? (
-              <View
-                style={{
-                  flexDirection: "row",
-                  flex: 1,
-                  borderRadius: 10,
-                  overflow: "hidden",
-                  borderWidth: 1,
-                  borderColor: name === selected ? "tomato" : "transparent",
-                }}
-              >
+          <View style={{ flex: 1, alignItems: "center" }}>
+            <TouchableOpacity
+              activeOpacity={1}
+              key={name}
+              style={{
+                width: "100%",
+                height: 60,
+                borderRadius: 10,
+                padding: 10,
+              }}
+              onPress={() => setSelected(name)}
+            >
+              {options ? (
+                <View
+                  style={{
+                    flexDirection: "row",
+                    flex: 1,
+                    borderRadius: 10,
+                    overflow: "hidden",
+                    borderWidth: 2,
+                    borderColor: name === selected ? "tomato" : colors.border,
+                  }}
+                >
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: options[0],
+                    }}
+                  />
+                  <View
+                    style={{
+                      flex: 1,
+                      backgroundColor: options[1],
+                    }}
+                  />
+                </View>
+              ) : (
                 <View
                   style={{
                     flex: 1,
-                    backgroundColor: colors[0],
+                    borderRadius: 10,
+                    backgroundColor,
+                    borderWidth: 2,
+                    borderColor: name === selected ? "tomato" : colors.border,
                   }}
                 />
-                <View
-                  style={{
-                    flex: 1,
-                    backgroundColor: colors[1],
-                  }}
-                />
-              </View>
-            ) : (
-              <View
-                style={{
-                  flex: 1,
-                  borderRadius: 10,
-                  backgroundColor,
-                  borderWidth: 1,
-                  borderColor: name === selected ? "tomato" : "transparent",
-                }}
-              />
-            )}
-          </TouchableOpacity>
+              )}
+            </TouchableOpacity>
+            <Text
+              style={{
+                color: colors.text,
+                textTransform: "capitalize",
+                fontWeight: "400",
+                fontSize: 14,
+              }}
+            >
+              {name}
+            </Text>
+          </View>
         );
       })}
     </View>
