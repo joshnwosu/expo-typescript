e54d108e8c0256886af45a6cf787a41ae33f5b6f
joshnwosu <joshuanwosu078@gmail.com>
2023-06-01 00:46:27 +0100

    update: setting up push notifications

---
+++ src/components/root/PushNotification.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+import * as Device from "expo-device";
+import * as Notification from "expo-notifications";
+
+const PushNotification = () => {
+  return (
+    <View>
+      <Text>PushNotification</Text>
+    </View>
+  );
+};
+
+export default PushNotification;
