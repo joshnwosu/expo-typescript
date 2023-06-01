a9546a3877389aaa080ce936cf726fbbaacb2f38
joshnwosu <joshuanwosu078@gmail.com>
2023-06-01 21:11:05 +0100

    update: fixed some ui bug

--- src/screens/LibraryScreen.tsx
+++ src/screens/LibraryScreen.tsx
@@ -15,41 +15,47 @@ import { increment, incrementByAmount } from "../features/counter/counterSlice";
 import ThemeContext from "../components/context/ThemeContext";
 import { IconName } from "../types/icon";
 import GroupCardList from "../components/layouts/group-card/GroupCardList";
-import { GroupCardItemProps } from "../types";
+import { GroupCardItemProps, GroupCardListProps } from "../types";
 
-const NOTES = [
+const NOTES: GroupCardListProps["data"] = [
   {
     name: "Notes",
-    count: 14,
+    message: 14,
     icon: "notes",
     icon_color: "#3F7DE8",
+    clickable: true,
   },
   {
     name: "Starred",
-    count: 5,
+    message: 5,
     icon: "star",
     icon_color: "#F0C422",
+    clickable: true,
   },
   {
     name: "Archive",
-    count: 7,
+    message: 7,
     icon: "archive",
     icon_color: "#67BE67",
+    clickable: true,
   },
   {
     name: "Recently Deleted",
-    count: 2,
+    message: 2,
     icon: "trash",
     icon_color: "#DE6058",
+    clickable: true,
   },
 ];
 
-const FOLDERS = [
-  { name: "Personal", count: 14, icon: "folder" },
-  { name: "Work", count: 5, icon: "folder" },
+const FOLDERS: GroupCardListProps["data"] = [
+  { name: "Personal", message: 14, icon: "folder" },
+  { name: "Work", message: 5, icon: "folder" },
 ];
 
-const LABELS = [{ name: "Inspiration", count: 8, icon: "tag" }];
+const LABELS: GroupCardListProps["data"] = [
+  { name: "Inspiration", message: 8, icon: "tag" },
+];
 
 export default function LibraryScreen() {
   const navigation = useNavigation();
@@ -65,8 +71,18 @@ export default function LibraryScreen() {
       showsVerticalScrollIndicator={false}
     >
       <GroupCardList data={NOTES} />
-      <GroupCardList title="Folders" data={FOLDERS} primary_color="#FFC04D" />
-      <GroupCardList title="Labels" data={LABELS} primary_color="#6d6f7a" />
+      <GroupCardList
+        title="Folders"
+        data={FOLDERS}
+        list_color="#FFC04D"
+        list_clickable={true}
+      />
+      <GroupCardList
+        title="Labels"
+        data={LABELS}
+        list_color="#6d6f7a"
+        list_clickable
+      />
     </ScrollView>
   );
 }
